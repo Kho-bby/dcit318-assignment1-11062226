b6b8fc1b63db0f850c40617039698c6896ff3c4d
kho-bby <cckonor@st.ug.edu.gh>
2025-08-04 23:37:17 +0000

    Ticket Price Calculator

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Choose an application");
+        Console.WriteLine("1. Grade Calculator");
+        Console.WriteLine("2. Ticket Price Calculator");
+        Console.WriteLine("3. Triangle Type Identifier");
+        Console.WriteLine("0. Exit");
+        Console.Write("Enter your choice (0-3): ");
+
+        string choice = Console.ReadLine();
+
+        switch (choice)
+        {
+            case "1":
+                GradeCalculator.Run();
+                break;
+           
+            default:
+                Console.WriteLine("Invalid choice. Please enter a number between 0 and 3.");
+                break;
+        }
+    }
+}
